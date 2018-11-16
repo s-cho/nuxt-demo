a5a130f43c17d33c79d26b74e5f6006a54204bb9
s-cho <s.cho.seraku@gmail.com>
2018-11-16 23:55:02 +0900

    formater

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,12 @@
   <section class="section">
     <!-- <Clock/> -->
     <h2><b>イベント一覧</b></h2>
-    <div class="event-item" v-for="event in eventList" v-bind:key="event.event_id">
+    <div 
+      v-for="event in eventList" 
+      :key="event.event_id" 
+      class="event-item">
       <datetime><small>{{ event.started_at }}</small></datetime><br>
-      {{ event.title}}
+      {{ event.title }}
     </div>
   </section>
 </template>
@@ -16,21 +19,21 @@ export default {
   components: { Clock },
   data() {
     return {
-      eventList:this.$store.state.eventList.data.events
+      eventList: this.$store.state.eventList.data.events
     }
   },
   mounted() {
     this.$store.dispatch('getEventList')
-  },
+  }
 }
 </script>
 <style lang="scss" scoped>
 .event-item {
   border-bottom: 1px solid gainsboro;
-  padding:10px;
+  padding: 10px;
 
   datetime {
-    color:gray;
+    color: gray;
   }
 }
 </style>
